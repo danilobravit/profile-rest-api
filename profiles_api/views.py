485399f92c5a4de20ee28f16349b249831d3fa4e
Danylo <danilobravit@gmail.com>
2021-11-08 22:54:57 +0100

    Added login API, user profile feed API

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
+from rest_framework.decorators import permission_classes
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework import viewsets
 from rest_framework.authentication import TokenAuthentication
 from rest_framework import filters
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
+from rest_framework.permissions import IsAuthenticated
 
 from . import serializers
 from . import models
@@ -106,3 +110,18 @@ class UserProfileViewSet(viewsets.ModelViewSet):
     permission_classes = (permissions.UpdateOwnProfile, )
     filter_backends = (filters.SearchFilter, )
     search_fields = ('name', 'email',)
+    
+class UserLoginApiView(ObtainAuthToken):
+    '''Habdle creating user authentication tokens'''
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
+    
+class UserProfileFeedViewSet(viewsets.ModelViewSet):
+    '''Handles creating, reading and updating profile feed items'''
+    authentication_classes = (TokenAuthentication, )
+    serializer_class = serializers.ProfileFeedItemSerializer
+    queryset = models.ProfileFeedItem.objects.all()
+    permission_classes = (permissions.UpdateOwnStatus, IsAuthenticated)
+    
+    def perform_create(self, serializer):
+        '''Sets the user prodile to the logged in user'''
+        serializer.save(user_profile=self.request.user)
